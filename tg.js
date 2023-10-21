52e3e5ea776b208952a81da441bed48ab78ea6bf
Gilad <freeslugs@gmail.com>
2023-10-21 17:07:35 +0300

    replace w better lib

--- tg.js
+++ tg.js
@@ -1,32 +1,19 @@
 
-import piexif from 'piexifjs';
-import fs from 'fs';
+import exifr from 'exifr' 
 
 import fetch from 'node-fetch';
 
 import TelegramBot from 'node-telegram-bot-api';
 const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, {polling: true});
-const getBase64DataFromJpegFile = filename => fs.readFileSync(filename).toString('binary');
-const getExifFromJpegFile = filename => piexif.load(getBase64DataFromJpegFile(filename));
-
-function debugExif(exif) {
-  for (const ifd in exif) {
-      if (ifd == 'thumbnail') {
-          const thumbnailData = exif[ifd] === null ? "null" : exif[ifd];
-          console.log(`- thumbnail: ${thumbnailData}`);
-      } else {
-          console.log(`- ${ifd}`);
-          for (const tag in exif[ifd]) {
-              console.log(`    - ${piexif.TAGS[ifd][tag]['name']}: ${exif[ifd][tag]}`);
-          }
-      }
-  }
-}
 
 // Listen for new incoming messages
 bot.on('message', async (msg) => {
   // Handle the incoming message
-  
+  if(msg.text) {
+    console.log('hello world')
+    console.log(msg.text)
+  }
+
   if(msg.photo) {
     bot.sendMessage(msg.chat.id, 'please attach as file to preserve exif data');
     // bot.sendChatAction(msg.chat.id,  "find_location")
@@ -34,90 +21,47 @@ bot.on('message', async (msg) => {
 
   if (msg.document) {
     const fileId = msg.document.file_id;
-    const fileUrl = `https://api.telegram.org/bot${bot.token}/getFile?file_id=${fileId}`;
 
-    try {
-      const response = await fetch(fileUrl);
-      const data = await response.json();
-      const filePath = data.result.file_path;
-      
-      console.log(`filePath: ${filePath}`);
+    const fileUrl = `https://api.telegram.org/bot${bot.token}/getFile?file_id=${fileId}`;
+    const response = await fetch(fileUrl);
+    const data = await response.json();
+    console.log(data)
+    const filePath = data.result.file_path;
+    const fileType = filePath.split('.').pop().toLowerCase();
+    
+    if (fileType === 'jpg' || fileType === 'jpeg' || fileType === 'png' || fileType === 'heic') {
+      console.log('photo');
 
       const imageResponse = await fetch(`https://api.telegram.org/file/bot${bot.token}/${filePath}`);
       const imageBuffer = await imageResponse.buffer();
-      // console.log(imageBuffer)
-
-      // console.log(typeof imageBuffer);
-
-      // const base64data = imageBuffer.toString('binary');
-      // const exifData = piexif.load(base64data)
-
-      // console.log(exifData)
       
-      // fs.writeFile('image.jpg', imageBuffer, (err) => {
-      //   if (err) {
-      //     console.error('Error downloading image:', err);
-      //   } 
-        
-        const exif = piexif.load(imageBuffer.toString('binary'))
-        // const exif = getExifFromJpegFile("image.jpg");
-        
-        const latitude = exif['GPS'][piexif.GPSIFD.GPSLatitude];
-        const latitudeRef = exif['GPS'][piexif.GPSIFD.GPSLatitudeRef];
-        const longitude = exif['GPS'][piexif.GPSIFD.GPSLongitude];
-        const longitudeRef = exif['GPS'][piexif.GPSIFD.GPSLongitudeRef];
-
-        if(latitude && longitude) {
-          console.log(`${latitude} ${latitudeRef}`, `${longitude} ${longitudeRef}`)
-          bot.sendLocation(msg.chat.id, `${latitude} ${latitudeRef}`, `${longitude} ${longitudeRef}`)
+      const exif = await exifr.parse(imageBuffer)
+      
+      const createdAt = new Date(exif.DateTimeOriginal).toLocaleString();
+      const updatedAt = new Date(exif.ModifyDate).toLocaleString();
+      
+      if(createdAt && updatedAt) {
+        let dateTimeMsg = `Created on ${createdAt}. `
+        if(createdAt != updatedAt) {
+          dateTimeMsg += `Updated on ${updatedAt}`
         }
-      // });
-    } catch (error) {
-      console.error(error);
+        bot.sendMessage(msg.chat.id, dateTimeMsg);
+      }
+      
+      const latitude = `${exif.latitude} ${exif.GPSLongitudeRef}`
+      const longitude = `${exif.longitude} ${exif.GPSLongitudeRef}`
+      
+      if(latitude && longitude) {
+        console.log(latitude, longitude)
+        bot.sendLocation(msg.chat.id, latitude, longitude)
+      }
+    
+    } else if (fileType === 'mp4' || fileType === 'avi' || fileType === 'mov') {
+      console.log('video');
+    } else {
+      console.log('unknown');
     }
-
-    // console.log(`fileUrl: ${fileUrl}`)
-
-    // https://api.telegram.org/bot<bot_token>/getFile?file_id=the_file_id
   }
-  
-  
 });
 
-console.log('Server has started.');  // Log that server has started
-
-
-
-
-// console.log(debugExif(exif))
-
-// const lat = palm1Exif.GPS[0] + palm1Exif.GPSLatitude[1]/60 + palm1Exif.GPSLatitude[2]/3600;
-// const lng = palm1Exif.GPSLongitude[0] + palm1Exif.GPSLongitude[1]/60 + palm1Exif.GPSLongitude[2]/3600;
-// console.log(`lat: ${lat}, lng: ${lng}`)
-
-// Show the latitudes and longitudes where the palm tree photos were taken 
-
-  // console.log("---------------------");
-  // console.log(`Latitude: ${latitude} ${latitudeRef}`);
-  // console.log(`Longitude: ${longitude} ${longitudeRef}\n`);
-
-
-
-
-// function debugExif(exif) {
-//   for (const ifd in exif) {
-//       if (ifd == 'thumbnail') {
-//           const thumbnailData = exif[ifd] === null ? "null" : exif[ifd];
-//           console.log(`- thumbnail: ${thumbnailData}`);
-//       } else {
-//           console.log(`- ${ifd}`);
-//           for (const tag in exif[ifd]) {
-//               console.log(`    - ${piexif.TAGS[ifd][tag]['name']}: ${exif[ifd][tag]}`);
-//           }
-//       }
-//   }
-// }
-
-
-// const imageExif = getExifFromJpegFile("image.jpg");
-// console.log(imageExif)
+console.log('Server has started.');
